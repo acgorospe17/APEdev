802e7bf260814cd4d2f94936a13da9af2f9771af
Andrew Gorospe <acgorospe@me.com>
2018-09-19 10:11:40 -0400

    mk driver
    template created

---
+++ v2/mK1000.py
@@ -0,0 +1,69 @@
+class mK1000():
+    def __init__():
+        pass
+
+    def get_sys_status():
+        pass
+
+    def mk_open():
+        pass
+
+    def close():
+        pass
+
+    def start_query():
+        pass
+
+    def stop_query():
+        pass
+
+    def hold():
+        pass
+
+    def ramp():
+        pass
+
+    def stop():
+        pass
+
+    def turn_cool_on():
+        pass
+
+    def get_turn_cool_status():
+        pass
+
+    def get_range():
+        pass
+
+    def set_range():
+        pass
+
+    def get_sensor_type():
+        pass
+
+    def get_pid():
+        pass
+
+    def set_pid():
+        pass
+
+    def get_current_temp():
+        pass
+
+    def get_tc_sensor_type():
+        pass
+
+    def get_tm_sensor_type():
+        pass
+
+    def set_tc_sensor_type():
+        pass
+
+    def set_tm_sensor_type():
+        pass
+
+    def get_out3_switch():
+        pass
+
+    def set_out3_switch():
+        pass
