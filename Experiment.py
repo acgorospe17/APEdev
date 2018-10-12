f6cb0b7b6e7cab08c232487751d1252cf7ce5666
jhardin4 <43216607+jhardin4@users.noreply.github.com>
2018-10-12 08:57:22 -0400

    Tray Implent

--- Experiment.py
+++ Experiment.py
@@ -1,10 +1,11 @@
 import Apparatus
 import Procedures_Toolpath
 import Procedures_Alignments
-import Procedures_Camera
+import Procedures_SampleTray
 import Executor
 import XlineTPGen as TPGen
 import FlexPrinterApparatus
+from Procedure import procedure
 
 MyApparatus = Apparatus.apparatus()
 MyExecutor = Executor.executor()
@@ -34,15 +35,6 @@ MyApparatus['devices']['TProbe']['AIaxis'] = 'ZZ2'
 MyApparatus['devices']['TProbe']['AIchannel'] = 0
 
 MyApparatus['devices']['camera'] = {'type':'Ueye_Camera', 'addresstype':'pointer', 'settle_time':5}
-
-MyApparatus.Connect_All(MyExecutor, simulation=True)
-
-
-AlignPrinter = Procedures_Alignments.Align(MyApparatus, MyExecutor)
-GenerateToolpath = Procedures_Toolpath.Generate_Toolpath(MyApparatus, MyExecutor)
-PrintToolpath = Procedures_Toolpath.Print_Toolpath(MyApparatus, MyExecutor)
-CameraTest = Procedures_Camera.Capture_Image(MyApparatus, MyExecutor)
-
 MyApparatus['devices']['gantry']['TProbe'] = {'axismask': {'Z': 'ZZ3'}}
 MyApparatus['information']['alignmentnames'].append('TProbe@mark')
 MyApparatus['information']['alignments']['TProbe@mark'] = {'X': '', 'Y': '', 'ZZ3': ''}
@@ -52,7 +44,7 @@ MyApparatus['devices']['gantry']['camera'] = {'axismask': {'Z': 'ZZ4'}}
 MyApparatus['information']['alignmentnames'].append('camera@mark')
 MyApparatus['information']['alignments']['camera@mark'] = {'X': '', 'Y': '', 'ZZ4': ''}
 
-AlignPrinter.Do({'primenoz': 'nstuff'})
+
 MyApparatus['information']['materials']['stuff']['density'] = 1.13
 MyApparatus['information']['toolpaths'] = {}
 MyApparatus['information']['toolpaths']['generator'] = TPGen.GenerateToolpath
@@ -61,6 +53,39 @@ MyApparatus['information']['toolpaths']['toolpath'] = [0]
 
 MyApparatus['information']['height_data'] = [0]
 
-GenerateToolpath.Do()
-PrintToolpath.Do({'toolpath': GenerateToolpath.requirements['target']['value']})
-#CameraTest.Do({'file':'Data\\test.tif', 'settle_time': 2, 'camera_name':'camera'})
+MyApparatus.Connect_All(MyExecutor, simulation=True)
+
+# Create instances of the procedures that will be used
+AlignPrinter = Procedures_Alignments.Align(MyApparatus, MyExecutor)
+BuildGrid = Procedures_SampleTray.Setup_XYGridTray(MyApparatus, MyExecutor)
+SampleGrid = Procedures_SampleTray.SampleTray(MyApparatus, MyExecutor)
+
+
+# Create procedure to do at each position in the sample grid
+class Sample(procedure):
+    def Prepare(self):
+        self.name = 'Sample'
+        self.gentp = Procedures_Toolpath.Generate_Toolpath(MyApparatus, MyExecutor)
+        self.printtp = Procedures_Toolpath.Print_Toolpath(MyApparatus, MyExecutor)
+    def Plan(self):
+        # Renaming useful pieces of informaiton
+
+        # Retreiving necessary device names
+
+        # Retrieving information from apparatus
+
+        # Getting necessary eprocs
+
+        # Assign apparatus addresses to procedures
+
+        # Doing stuff
+        self.gentp.Do()
+        self.printtp.Do({'toolpath': self.gentp.requirements['target']['value']})
+
+
+sample = Sample(MyApparatus, MyExecutor)
+
+# Do the experiment
+AlignPrinter.Do({'primenoz': 'nstuff'})
+BuildGrid.Do({'trayname': 'test_tray', 'samplename': 'bleh', 'xspacing': 1, 'xsamples': 2, 'yspacing': 1, 'ysamples': 3})
+SampleGrid.Do({'trayname': 'test_tray', 'procedure': sample})
